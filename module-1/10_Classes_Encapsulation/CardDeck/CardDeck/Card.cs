de43434c5f845acca0a5a502b9ea508f2d4d3a80
John Fulton <john@techelevator.com>
2019-09-27 12:10:19 -0400

    module-1/10_Classes_Encapsulation/ in class changes

---
+++ module-1/10_Classes_Encapsulation/CardDeck/CardDeck/Card.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardDeck
+{
+    class Card
+    {
+        public string Suit { get; set; }
+        public string Value { get; set; }
+        public bool IsFaceUp { get; set; }
+
+        public Card ()
+        {
+            IsFaceUp = false;
+        }
+
+        public Card(string value, string suit)
+        {
+            Value = value;
+            Suit = suit;
+            IsFaceUp = false;
+        }
+
+        public bool Flip()
+        {
+            IsFaceUp = !IsFaceUp;
+            return IsFaceUp;
+        }
+
+        public override string ToString()
+        {
+            return Value + " " + Suit;
+        }
+    }
+}
