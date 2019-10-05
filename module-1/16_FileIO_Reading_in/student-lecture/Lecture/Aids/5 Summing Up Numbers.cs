84321e0f05a4b561765f1105ade7cbc9b44ed374
John Fulton <john@techelevator.com>
2019-10-05 19:52:27 -0400

    module-1/16_FileIO_Reading_in/

---
+++ module-1/16_FileIO_Reading_in/student-lecture/Lecture/Aids/5 Summing Up Numbers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Lecture.Aids
+{
+    public static class SummingUpNumbers
+    {
+        public static void ReadFile()
+        {
+            // Reading in a file of numbers
+            string folder = Environment.CurrentDirectory; //an ugly name
+            //string folder = "c:\\niceplace"; //or you can create a nice place
+
+            string filename = "numbers.txt";
+            // get the full path
+            string fullpath = Path.Combine(folder, filename);
+
+            int sum = 0;
+            using (StreamReader sr = new StreamReader(fullpath))
+            {
+                // Read until we get to the end of the file
+                while (!sr.EndOfStream)
+                {
+                    // TODO - Changes inside read loop
+                    // Read the line
+                    // Convert to a number
+                    // Add to Sum
+                }
+            }
+            
+            Console.WriteLine("The sum is " + sum);
+        }
+    }
+}
