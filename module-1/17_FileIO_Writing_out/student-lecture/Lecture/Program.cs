e9f68b60ff6a528107b011790e5cfe04065808a0
John Fulton <john@techelevator.com>
2019-10-07 19:11:03 -0400

    module-1/17_FileIO_Writing_out/

---
+++ module-1/17_FileIO_Writing_out/student-lecture/Lecture/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Lecture.Aids;
+
+namespace Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Here are a few examples of reading in a file and writing out values
+            // to demonstrate their value.
+            //ReadingInFiles.ReadACharacterFile();
+            //ReadingInFiles.ReadInACSVFile();  
+            WritingTextFiles.WritingAFile();
+            //BinaryFileWriter.WritePrimitiveValues();          
+            //BinaryImageManipulator.ReadFileIn();
+            //PerformanceDemo.SlowPerformance();
+            //PerformanceDemo.FastPerformance();
+
+            Console.Write("Press enter to finish");
+            Console.ReadLine();
+        }
+    }
+}
