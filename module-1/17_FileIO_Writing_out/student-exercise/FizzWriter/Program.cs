03a06d42faad5444779f279bd7e3610037153797
Basal Ali <basalali@hotmail.com>
2019-10-08 17:24:52 -0400

    hwm 17 complete

--- module-1/17_FileIO_Writing_out/student-exercise/FizzWriter/Program.cs
+++ module-1/17_FileIO_Writing_out/student-exercise/FizzWriter/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
-
+using System.IO;
 namespace FizzWriter
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            string directory = @"C:\goodPlace";
+            string fileName = "FizzBuzz.txt";
+            string fullPath = Path.Combine(directory, fileName);
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(fullPath, false))
+                {
+                    for (int i = 1; i <= 300; i++)
+                    {
+                        string result = "";
+                        if (i % 3 == 0 && i % 5 == 0)
+                        {
+                            result = "FizzBuzz";
+
+                        }
+                        else if (i % 5 == 0 || i.ToString().Contains("5"))
+                        {
+
+                            result = "Buzz";
+                        }
+                        else if (i % 3 == 0 || i.ToString().Contains("3"))
+                        {
+                            result = "Fizz";
+                        }
+                        else
+                        {
+                            result = i.ToString();
+                        }
+
+                        sw.WriteLine(result);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error! Unable to write to file.");
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
