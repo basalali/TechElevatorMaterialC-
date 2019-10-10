41f70d6ae3514febf80b6b272c81c709ff27cb90
John Fulton <john@techelevator.com>
2019-10-10 11:07:08 -0400

    module-1/18_Review/PetInfo\ V2-with-johns-changes for reading file

--- module-1/18_Review/PetInfo V2-with-johns-changes/PetInfo/DataAccess.cs
+++ module-1/18_Review/PetInfo V2-with-johns-changes/PetInfo/DataAccess.cs
@@ -37,6 +37,34 @@ namespace PetInfo
         {
             List<Pet> result = new List<Pet>();
 
+            try
+            {
+
+
+                using (StreamReader sr = new StreamReader("data.txt"))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string[] values = line.Split('|');
+
+                        Pet item = new Pet();
+
+                        item.Name = values[0];
+                        item.Type = values[1];
+                        item.FamilyName = values[2];
+                        item.AgeInMonths = int.Parse(values[3]);
+
+                        result.Add(item);
+                    }
+                }
+            }
+
+            catch (Exception ex)
+            {
+                result = new List<Pet>();
+            }
+
             return result;
         }
 
