73644efc974eb96af7d0371d052d4c570fef082e
Basal Ali <basalali@hotmail.com>
2019-10-19 22:42:52 -0400

    mod 2/hwm 5 complete, hire/birth dates fields still at 0.

--- module-2/05_Database_Design/student-exercise/projects.sql
+++ module-2/05_Database_Design/student-exercise/projects.sql
@@ -1,11 +1,11 @@
-CREATE DATABASE ProjectManager;
+CREATE DATABASE ProjectManager1;
  -- if you include identity, it genereates for you. PK is unique and not null.
  BEGIN TRANSACTION
 
 CREATE TABLE Department
 (
 	department_id int IDENTITY,
-	department_name nvarchar(60) NOT NULL,
+	department_name nvarchar(100) NOT NULL,
 	CONSTRAINT pk_depart_ID PRIMARY KEY (department_id),
 
 )
@@ -13,14 +13,14 @@ CREATE TABLE Department
 CREATE TABLE employee
 (
     employee_id int IDENTITY,
-    job_title nvarchar(60) NOT NULL,
-	last_name nvarchar(60) NOT NUll,
-	first_name nvarchar(60) NOT NULL,
-	gender nvarchar(60) NOT NULL,
+    job_title nvarchar(100) NOT NULL,
+	last_name nvarchar(100) NOT NUll,
+	first_name nvarchar(100) NOT NULL,
+	gender nvarchar(100) NOT NULL,
 	date_of_birth smallint NOT NULL,
 	date_of_hire smallint NOT NULL,
-	department_id int NOT NULL,
-	employee_home_department nvarchar(60) NOT NULL,
+	department_id int,
+	employee_home_department nvarchar(100) NOT NULL,
 	CONSTRAINT pk_employee_id PRIMARY KEY (employee_id),
 	CONSTRAINT fk_department_id FOREIGN KEY (department_id) REFERENCES department(department_id),
 
@@ -30,17 +30,15 @@ CREATE TABLE employee
 CREATE TABLE Project
 (
 	project_id int IDENTITY,
-	project_name nvarchar(60) NOT NULL,
-	employee_id int NOT NULL,
+	project_name nvarchar(100) NOT NULL,
+	employee_id int,
 	CONSTRAINT pk_project_id PRIMARY KEY (project_id),
 	CONSTRAINT fk_employee_id FOREIGN KEY (employee_id) REFERENCES employee(employee_id)
 
 )
-COMMIT 
 
-SELECT * FROM employee
-SELECT * FROM Department
-SELECT * FROM Project
+
+
 ------- Department Inserts -------
 INSERT Department (department_name)
 VALUES ('Finance')
@@ -55,38 +53,38 @@ INSERT employee(job_title, last_name, first_name, gender, date_of_birth, date_of
 VALUES ('President', 'Ali', 'Basal', 'Male', 10/10/1856, 08/15/15, 'Finance')
 
 INSERT employee(job_title, last_name, first_name, gender, date_of_birth, date_of_hire, employee_home_department)
-VALUES ('President', 'Ali', 'Layla', 'Female', 10/10/1990, 08/15/19, 'Marketing')
+VALUES ('VP of Marketing', 'Ali', 'Layla', 'Female', 10/10/1990, 08/15/19, 'Marketing')
 
 INSERT employee(job_title, last_name, first_name, gender, date_of_birth, date_of_hire, employee_home_department)
-VALUES ('President', 'Smith', 'John', 'Male', 09/18/1985, 08/15/10, 'Operations')
+VALUES ('Director of Ops', 'Smith', 'John', 'Male', 09/18/1985, 08/15/10, 'Operations')
 
 INSERT employee(job_title, last_name, first_name, gender, date_of_birth, date_of_hire, employee_home_department)
-VALUES ('President', 'Ahmed', 'Jama', 'Male', 03/14/1995, 08/15/18, 'Finance')
+VALUES ('Analyst', 'Ahmed', 'Jama', 'Male', 03/14/1995, 08/15/18, 'Finance')
 
 INSERT employee(job_title, last_name, first_name, gender, date_of_birth, date_of_hire, employee_home_department)
-VALUES ('President', 'Watson', 'Jessica', 'FMale', 10/10/1800, 08/15/97, 'Marketing')
+VALUES ('Marketing Manager', 'Watson', 'Jessica', 'FMale', 10/10/1800, 08/15/97, 'Marketing')
 
 INSERT employee(job_title, last_name, first_name, gender, date_of_birth, date_of_hire, employee_home_department)
-VALUES ('President', 'Alba', 'Jessica', 'Female', 10/10/1856, 08/15/15, 'Operations')
+VALUES ('Warehouse Manager', 'Alba', 'Jessica', 'Female', 10/10/1856, 08/15/15, 'Operations')
 
 INSERT employee(job_title, last_name, first_name, gender, date_of_birth, date_of_hire, employee_home_department)
-VALUES ('President', 'Ali', 'Basal', 'Male', 10/10/1856, 08/15/15, 'Finance')
+VALUES ('Cusomter Rep', 'Franky', 'Frank', 'Male', 10/10/1956, 06/15/11, 'Finance')
 
 INSERT employee(job_title, last_name, first_name, gender, date_of_birth, date_of_hire, employee_home_department)
-VALUES ('President', 'Ali', 'Basal', 'Male', 10/10/1856, 08/15/15, 'Operations')
+VALUES ('Senior Inventory Engineer', 'Aden', 'Dahir', 'Male', 10/10/1993, 01/01/18, 'Operations')
 
 --------- Project Inserts
 INSERT Project(project_name)
-VALUES ('')
+VALUES ('Database Crossover')
 
 INSERT Project(project_name)
-VALUES ('')
+VALUES ('Target Acquisition')
 
 INSERT Project(project_name)
-VALUES ('')
+VALUES ('Feeding The Homeless')
 
 INSERT Project(project_name)
-VALUES ('')
+VALUES ('Merge Factor')
 
 COMMIT 
 
