8a88e7e64f7ba1b15c0fe77a5715febfdc3d3a78
John Fulton <john@techelevator.com>
2019-11-01 12:13:31 -0400

    module-3/05-MVC-Views-Part-3/ with in-class changes

--- module-3/05-MVC-Views-Part-3/student-lecture-with-johns-changes/TechElevator.Web/Startup.cs
+++ module-3/05-MVC-Views-Part-3/student-lecture-with-johns-changes/TechElevator.Web/Startup.cs
@@ -5,24 +5,12 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace TechElevator.Web
 {
     public class Startup
     {
-        //beginning of insert
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        //end of insert
-
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -46,35 +34,6 @@ namespace TechElevator.Web
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
 
-            //insert starts here
-            SqlConnectionString = Configuration["ConnectionStrings:SqlConnectionString"];
-            FilePath = Configuration["ConnectionStrings:FilePath"];
-            Source = Configuration["ConnectionStrings:Source"];
-            //insert ends here
         }
-    
-        //insert starts here
-
-        public static string SqlConnectionString
-        {
-            get;
-            private set;
-        }
-
-        public static string FilePath
-        {
-            get;
-            private set;
-        }
-
-        public static string Source
-        {
-            get;
-            private set;
-        }
-
-        //insert end here
-
-
-}
+    }
 }
