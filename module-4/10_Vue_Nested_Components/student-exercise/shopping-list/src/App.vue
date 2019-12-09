79b4e6b64e8fdf459b65724942c7ceb24d9a66c1
Basal Ali <basalali@hotmail.com>
2019-12-08 23:44:53 -0500

    final hwm complete

--- module-4/10_Vue_Nested_Components/student-exercise/shopping-list/src/App.vue
+++ module-4/10_Vue_Nested_Components/student-exercise/shopping-list/src/App.vue
@@ -1,51 +1,84 @@
 <template>
-  <div id="app">
-    <h1>Nested Components Exercise</h1>
-
-  </div>
+<div id="app">
+<h1>Nested Components Exercise</h1>
+<shopping-search v-on:filter-tasks="handleSearch"></shopping-search>
+<shopping-list title="Groceries List" v-bind:items="groceries" v-bind:search="search"></shopping-list>
+<shopping-list title="My Costco List" v-bind:items="costco" v-bind:search="search"></shopping-list>
+<shopping-list title="My Target List" v-bind:items="target" v-bind:search="search"></shopping-list>
+</div>
 </template>
 
 <script>
+import ShoppingSearch from "@/components/ShoppingSearch";
+import ShoppingList from "@/components/ShoppingList";
 
 export default {
-  name: 'app',
-  components: {
-
-  },
-  data() {
-    return {
-      
-    }
-  },
-  methods: {
-
-  }
+name: "app",
+components: {
+ShoppingSearch,
+ShoppingList
+},
+data() {
+return {
+search: '',
+groceries: [
+{ id: 1, name: "Oatmeal", completed: false },
+{ id: 2, name: "Milk", completed: false },
+{ id: 3, name: "Banana", completed: false },
+{ id: 4, name: "Strawberries", completed: false },
+{ id: 5, name: "Lunch Meat", completed: false }
+],
+costco: [
+{ id: 1, name: "Paper Towels", completed: false },
+{ id: 2, name: "Toilet Paper", completed: false },
+{ id: 3, name: "Bottled Water", completed: false },
+{ id: 4, name: "Paper Plates", completed: false },
+{ id: 5, name: "Pepsi", completed: false }
+],
+target: [
+{ id: 1, name: "Baby Food", completed: false },
+{ id: 2, name: "Candles", completed: false },
+{ id: 3, name: "Aspirin", completed: false },
+{ id: 4, name: "Dish Soap", completed: false },
+{ id: 5, name: "Laundry Soap", completed: false }
+]
+};
+},
+methods: {
+handleSearch(query) {
+this.search = query;
 }
+}
+};
 </script>
 
 <style>
 body {
-  background: rgb(61,201,164);
-  background: radial-gradient(circle, rgba(61,201,164,1) 50%, rgba(92,133,120,1) 100%);
+background: rgb(61, 201, 164);
+background: radial-gradient(
+circle,
+rgba(61, 201, 164, 1) 50%,
+rgba(92, 133, 120, 1) 100%
+);
 }
 #app {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 20px;
+display: grid;
+grid-template-columns: 1fr 1fr 1fr;
+grid-gap: 20px;
 }
 h1 {
-  grid-row: 1;
-  grid-column: 1 / 4;
-  text-align:center;
-  color:#f2f2f2;
-  text-transform: uppercase;
-  font-family: 'Roboto Condensed', sans-serif;
+grid-row: 1;
+grid-column: 1 / 4;
+text-align: center;
+color: #f2f2f2;
+text-transform: uppercase;
+font-family: "Roboto Condensed", sans-serif;
 }
-.search { 
-  grid-column: 2 / 3;
-  grid-row: 2;
+.search {
+grid-column: 2 / 3;
+grid-row: 2;
 }
 .shopping-list {
-  grid-row: 3;
+grid-row: 3;
 }
 </style>
